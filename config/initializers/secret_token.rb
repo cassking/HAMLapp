059ed5fdd9c784658f533ac92292f4baf3f91592
Cassandra King <cking@Administrators-MacBook-Pro-7.local>
2014-01-07 12:29:56 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HAMLapp::Application.config.secret_key_base = 'f5d26349760ababc5ff170b446e516c2fcf5274e172e7b81cc7bb6f0831fcc4dbb8c62afb6dbe3a8583f9545c3c94df1bd5b7afd794866bab59d59399d4f557a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HAMLapp::Application.config.session_store :cookie_store, key: '_HAMLapp_session'
