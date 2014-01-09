2dca40b06653d16daf68840b490415fd138abb47
Cassandra King <cking@Administrators-MacBook-Pro-7.local>
2014-01-09 12:03:33 -0500

    add jquery practice file

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,1 +1,34 @@
 //comment here
+//Testing a selection to be sure it exists on page
+$(document).ready(function() {
+	console.log($('#celebs tr').length + 'elements!');	
+});
+
+$(document).ready(function() {
+ console.log($('#celebs tbody tr:even').length + 'even rows in the table');
+});
+
+//selectors, this sselects all these
+//$('p, div, span, input')
+
+//reading css properties
+$(document).ready(function(){
+	var fontSize = $('#celebs tbody tr:first').css('font-size');
+	var tableRows = ($('#celebs tbody tr').length);
+	console.log(fontSize + ' is the font color' + tableRows + ' this number rows')
+
+});
+
+//targetting the css the long way
+$(document).ready(function() {
+	$('#celebs tbody tr:even').css('background-color', '#dddddd');
+	$('#celebs tbody tr:even').css('color', '#039');
+});
+
+//targetting css using object literals
+$(document).ready(function() {
+	$('#celebs tbody tr:odd').css(
+		{'background-color':'#000', 
+		'color':'#eee'}
+		);
+});
