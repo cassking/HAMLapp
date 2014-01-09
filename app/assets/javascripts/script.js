a2c89e92575e3d982eed3012318f5728bad1ddf6
Cassandra King <cking@Administrators-MacBook-Pro-7.local>
2014-01-09 16:20:24 -0500

    add jquery for show, hide

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,4 +1,5 @@
 //comment here
+// TASK 1
 //Testing a selection to be sure it exists on page
 $(document).ready(function() {
 	console.log($('#celebs tr').length + 'elements!');	
@@ -10,6 +11,7 @@ $(document).ready(function() {
 
 //selectors, this sselects all these
 //$('p, div, span, input')
+// TASK 2
 
 //reading css properties
 $(document).ready(function(){
@@ -19,16 +21,58 @@ $(document).ready(function(){
 
 });
 
+// TASK 3
 //targetting the css the long way
-$(document).ready(function() {
-	$('#celebs tbody tr:even').css('background-color', '#dddddd');
-	$('#celebs tbody tr:even').css('color', '#039');
+		// $(document).ready(function() {
+		// 	$('#celebs tbody tr:even').css('background-color', '#dddddd');
+		// 	$('#celebs tbody tr:even').css('color', '#039');
+		// });
+// TASK 4
+//targetting css using object literals, but this adds styles inline not best practice
+		// $(document).ready(function() {
+		// 	$('#celebs tbody tr:odd').css(
+		// 		{'background-color':'#000', 
+		// 		'color':'#eee'}
+		// 		);
+		// });
+// TASK 5
+//adding and removing css, moving css to the stylesheet
+
+$(document).ready(function()
+{
+	$('#celebs tbody tr:even').addClass('zebra');
+
 });
 
-//targetting css using object literals
-$(document).ready(function() {
-	$('#celebs tbody tr:odd').css(
-		{'background-color':'#000', 
-		'color':'#eee'}
-		);
+//TASK 6 hiding elements
+//$('#hideButton').click(function(){
+	//$('#comment').hide();
+//});
+$(document).ready(function(){
+	$('#hideButton').click(function(){
+	    $('.disclaimer').hide();
+	});
 });
+//TASK 7 , this: use this to refer to the element that trigggered event
+$(document).ready(function(){
+	$('#celebs').click(function() {
+		$(this).append("<p>this is text</p>");
+	});
+});
+//TASK 8 show the disclaimer if hidden by mistake
+
+$(document).ready(function(){
+	$('#showButton').click(function(){
+		$('.disclaimer').show();
+	});
+
+});
+
+
+
+
+
+
+
+
+
